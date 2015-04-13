930402f0b5b14ba7b3dbf4475be2d2358614d590
Michael Becco <e4e6d4d5@gmail.com>
2015-04-13 14:07:17 -0700

    Lesson 2 Push-

--- app.js
+++ app.js
@@ -5,6 +5,18 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
   var gems = [
     {
       name: "Ruby",
@@ -12,6 +24,11 @@
       description: ". . .",
       canPurchase: true,
       soldOut: false,
+      images: [
+        {
+          full: 'ruby.jpeg'
+        }
+      ]
     },
     {
       name: "Pentagonal Gem",
